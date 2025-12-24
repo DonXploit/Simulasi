ce666d30d288a9b8769acda96aa6323cb153c0ad
DonXploit <149055044+DonXploit@users.noreply.github.com>
2025-12-24 10:12:23 +0700

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Opsi konfigurasi khusus Next.js dapat ditambahkan di sini
+  // Misalnya: output: 'export' untuk static HTML export (jika dibutuhkan)
+  // images: { unoptimized: true } (jika menggunakan output: 'export')
+};
+
+export default nextConfig;
