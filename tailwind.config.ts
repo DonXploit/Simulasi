05100d68f2c1d8388678c9214dc7efc143e4ecf6
DonXploit <149055044+DonXploit@users.noreply.github.com>
2025-12-24 10:30:49 +0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,13 +2,11 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    // Ini pola yang harus Anda gunakan untuk memastikan Vercel memindai folder src/
+    './src/**/*.{js,ts,jsx,tsx,mdx}', 
   ],
   theme: {
     extend: {
-      // Anda dapat menambahkan tema kustom, font, atau warna di sini
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
